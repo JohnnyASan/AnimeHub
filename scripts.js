d5ee04024924b1fe258df267ac919cab591b0217
Johnny <johnanthonysanabria@gmail.com>
2019-02-15 18:31:44 -0700

    added my custom styling

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+const app = document.getElementById('root');
+console.log(app);
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
 var request = new XMLHttpRequest();
 
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
@@ -8,7 +20,18 @@ request.onload = function() {
 
   if (request.status >= 200 && request.status < 400) {
     movieData.forEach(movie => {
-      console.log(movie.title);
+      
+      const block = document.createElement('div');
+      block.setAttribute('class', 'block');
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+      container.appendChild(block);
+      block.appendChild(h1);
+      block.appendChild(p);
     });
   } else {
     console.log('error');
@@ -16,15 +39,3 @@ request.onload = function() {
 }
 
 request.send();
-
-const app = document.getElementById('root');
-console.log(app);
-
-const logo = document.createElement('img');
-logo.src = 'img/StudioGhiblilogo.png';
-
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-
-app.appendChild(logo);
-app.appendChild(container);
